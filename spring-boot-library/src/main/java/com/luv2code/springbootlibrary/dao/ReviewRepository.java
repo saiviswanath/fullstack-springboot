785f964c65add4ff5f21509df8b277af99a037b2
viswanath.palaparthi <viswanath.palaparthi@gmail.com>
2022-12-16 18:56:49 +0530

    review service

--- spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
+++ spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
@@ -9,4 +9,6 @@ import com.luv2code.springbootlibrary.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 	Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
+	
+	Review findByUserEmailAndBookId(String userEmail, Long bookId);
 }
