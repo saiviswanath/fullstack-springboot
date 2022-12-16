38574614006011095f1a44025b0c0f511c60da40
viswanath.palaparthi <viswanath.palaparthi@gmail.com>
2022-12-16 21:42:42 +0530

    history svc

--- spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
+++ spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
@@ -13,8 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.luv2code.springbootlibrary.dao.BookRepository;
 import com.luv2code.springbootlibrary.dao.CheckoutRepository;
+import com.luv2code.springbootlibrary.dao.HistoryRepository;
 import com.luv2code.springbootlibrary.entity.Book;
 import com.luv2code.springbootlibrary.entity.Checkout;
+import com.luv2code.springbootlibrary.entity.History;
 import com.luv2code.springbootlibrary.responsemodels.ShelfCurrentLoansResponse;
 
 @Service
@@ -23,10 +25,14 @@ public class BookService {
     private BookRepository bookRepository;
 
     private CheckoutRepository checkoutRepository;
+    
+    private HistoryRepository historyRepository;
 
-	public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+	public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository,
+			HistoryRepository historyRepository) {
 		this.bookRepository = bookRepository;
 		this.checkoutRepository = checkoutRepository;
+		this.historyRepository = historyRepository;
 	}
 	
 	public Book checkoutBook(String userEmail, Long bookId) throws Exception {
@@ -116,6 +122,18 @@ public class BookService {
 
         bookRepository.save(book.get());
         checkoutRepository.deleteById(validateCheckout.getId());
+        
+        History history = new History(
+                userEmail,
+                validateCheckout.getCheckoutDate(),
+                LocalDate.now().toString(),
+                book.get().getTitle(),
+                book.get().getAuthor(),
+                book.get().getDescription(),
+                book.get().getImg()
+        );
+
+        historyRepository.save(history);
     }
     
     public void renewLoan(String userEmail, Long bookId) throws Exception {
